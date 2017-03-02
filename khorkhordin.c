c1ab3445dab78a66265c443ebc6308b6a3e747a3
hyariks <yhorhordin124@gmail.com>
2017-03-03 00:44:26 +0300

    Update khorkhordin.c

--- khorkhordin.c
+++ khorkhordin.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 
 struct reg {
     int num;
@@ -12,23 +10,18 @@ struct reg {
 int main(int argc, char **argv) {
 
     struct reg p;
-    FILE *fp;
-    
-    fp = fopen(argv[1], "rb\n");
+    FILE *fp = fopen(argv[1], "rb");
     
     if (fp == NULL) {
         printf("Can't open file\n");
-        return EXIT_FAILURE;
+        return NULL;
     }
 
     while (fread(&p , sizeof(struct reg),1, fp))
         printf("%d %s %f\n", p.num, p.str, p.numf);
 
-    fseek(fp, 0, SEEK_END);
     int fileSize = ftell(fp);
-    rewind(fp);
     printf("Size in bytes : %d\n", fileSize);
 
-    fclose(fp);
-    return EXIT_SUCCESS;
+    return 0;
 }
